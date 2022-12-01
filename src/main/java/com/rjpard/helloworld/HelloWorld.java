5eabb3160f55a0dfb33bd3c9dd0498dd860be7d9
rjpatraining <rjpatraining@gmail.com>
2022-12-01 08:46:14 -0600

    First Commit

---
+++ src/main/java/com/rjpard/helloworld/HelloWorld.java
@@ -0,0 +1,9 @@
+package com.rjpard.helloworld;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World...! I am back to Java.");
+    }
+}
